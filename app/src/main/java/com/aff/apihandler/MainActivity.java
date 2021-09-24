aaf71308eab261a23a35d6418da71b0004732ba5
afrizzal <apratama8@gmail.com>
2021-09-25 04:40:40 +0700

    add MainActivity

--- app/src/main/java/com/aff/apihandler/MainActivity.java
+++ app/src/main/java/com/aff/apihandler/MainActivity.java
@@ -6,9 +6,21 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressDialog progressDialog;
+    private TextView tvShow;
+    private Button btnRefresh;
+
+    private String apiUrl = "https://api.kawalcorona.com/indonesia";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        tvShow = findV
     }
 }
